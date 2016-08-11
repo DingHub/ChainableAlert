e584f605eee5a2620500e81eb7fed19d72b13f41
admin <love-nankai@163.com>
2016-08-11 18:52:53 +0800

    Chainable use of Alert.

---
+++ ChainableAlert/ViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ViewController.swift
+//  ChainableAlert
+//
+//  Created by admin on 16/8/11.
+//  Copyright © 2016年 Ding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIAlertViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    @IBAction func showAlert(sender: AnyObject) {
+        
+        self.actionSheet("Title", message: "message")
+            .normalButton("normal1") {
+                print("normal1")
+            }
+            .normalButton("normal2")
+            .normalButton("normal3") {
+                print("normal3")
+            }
+            .destructiveButton("destructive1") {
+                print("destructive1")
+            }
+            .destructiveButton("destructive2") {
+                print("destructive2")
+            }
+            .cancleButton("cancle") {
+                print("canceled")
+            }
+            .show(fromPosition: (20, 30),
+                     animated: true) {
+                        print("showd!")
+        }
+        
+        /*
+        ChainableAlert
+            .actionSheet("Title", message: "message")
+            .normalButton("normal1") {
+                print("normal1")
+            }
+            .normalButton("normal2")
+            .normalButton("normal3") {
+                print("normal3")
+            }
+            .destructiveButton("destructive1") {
+                print("destructive1")
+            }
+            .destructiveButton("destructive2") {
+                print("destructive2")
+            }
+            .cancleButton("cancle") {
+                print("canceled")
+            }
+            .show(
+                fromPosition: (20, 30),
+                  animated: true) {
+                    print("showd!")
+        }
+         */
+
+        
+
+    }
+    
+    func alertView(alertView: UIAlertView, clickedButtonAtIndex buttonIndex: Int) {
+        print(buttonIndex)
+    }
+
+}
+
---
+++ Source/UIViewController+ChainableAlert.swift
@@ -0,0 +1,71 @@
+//
+//  UIViewController+ChainableAlert.swift
+//  ChainableAlert
+//
+//  Created by admin on 16/8/11.
+//  Copyright © 2016年 Ding. All rights reserved.
+//
+
+import UIKit
+
+var chainableAlert: ChainableAlert?
+extension UIViewController {
+    // Mark: init
+    
+    /**
+     Create alert, actually UIAlertView if the device is below iOS 8 or UIAlertController for iOS 8 and later
+     */
+    public func alert(titile: String?, message: String?) -> UIViewController {
+        chainableAlert = ChainableAlert.alert(titile, message: message)
+        return self
+    }
+    /**
+     Create alert, actually UIActionSheet if the device is below iOS 8 or UIAlertController for iOS 8 and later
+     */
+    public func actionSheet(titile: String?, message: String?) -> UIViewController {
+        chainableAlert = ChainableAlert.actionSheet(titile, message: message)
+        return self
+    }
+    
+    // Mark: add buttons
+    
+    /**
+     Add a normal button to the alert
+     */
+    public func normalButton(title: String, handler: AlertButtonAction? = nil) -> UIViewController {
+        chainableAlert?.normalButton(title, handler: handler)
+        return self
+    }
+    
+    /**
+     Add a destructive button to the alert, we can add more than 1 for iOS 8 and later, BUT, if below iOS 8, this func will do nothing for alertView, and only one destructive button will added for actionSheet
+     */
+    public func destructiveButton(title: String, handler: AlertButtonAction? = nil) -> UIViewController {
+        chainableAlert?.destructiveButton(title, handler: handler)
+        return self
+    }
+    
+    /**
+     Add a cancel button to the alert, the most number of cancel button is 1
+     */
+    public func cancleButton(title: String, handler: AlertButtonAction? = nil) -> UIViewController {
+        chainableAlert?.cancleButton(title, handler: handler)
+        return self
+    }
+    
+    // Mark: show
+    
+    /**
+     Show the alert
+     - parameter fromPosition:   If UIAlertController used, and style is action sheet, and device is iPad, we can set the source point for the popover controller
+     - parameter animated:       If below iOS 8 , no use.
+     - parameter completion:     If below iOS 8 , no use.
+     */
+    public func show(fromPosition position:(x: CGFloat, y: CGFloat)? = nil, animated:Bool, completion: (() -> Void)? = nil) {
+        if let alert = chainableAlert {
+            alert.show(self, fromPosition: position, animated: animated) {
+                completion?()
+            }
+        }
+    }
+}
